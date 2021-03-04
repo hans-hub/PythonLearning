2107c1c235657067ccd3aa6bb2370576221f20cb
Baffour Hans <baffourhans@gmail.com>
2021-03-05 06:04:04 +0800

    final chapter

--- CHAPTER4.py
+++ CHAPTER4.py
@@ -212,3 +212,46 @@ def outer_func(a,b):
         return c + d
     return inner_func(a,b)
 
+#the use of the lambda functions
+#the function capitalizes the list of words and appends an exclamation point
+
+def edit_story(words,func):
+    for word in words:
+        print(func(word))
+stairs = ['thud','meow','thud','hiss']
+def enliven(word):
+    return word.capitalize() + "!"
+def upper(word):
+    return word.upper() + "@"
+
+#using the lambda in place of the enliven function
+# edit_story(stairs, lambda word: word.capitalize() + '!')
+
+
+
+#DECORATORS
+def document_it(func):
+    def new_func(*args,**kwargs):
+        print('Runnning function:',func._name_)
+        print('positional arguments:',args)
+        print('keyword arguments:',kwargs)
+        result = func(*args, **args)
+        print('result:',result)
+        return result
+    
+              
+
+
+#catching errors using try and except
+short_list = [1, 2, 3]
+while True:
+    value = input('Position [q to quit]? ')
+    if value == 'q':
+        break
+    try:
+        position = int(value)
+        print(short_list[position])
+    except IndexError as err:
+        print('Bad index:', position)
+    except Exception as other:
+        print('Something else broke:', other)
